c1d0d8c9926fd65a66bf8d8db5a0eb37a1dde02a
Ravi Narayan <ravi.narayan09@gmail.com>
2022-06-12 14:26:49 -0700

    15

--- apps.js
+++ apps.js
@@ -18,23 +18,25 @@ fetch(urlAPI)
 function displayEmployees(employeeData) {
 
 employees = employeeData;
-
+///Store the employee HTML as we create it
 let employeeHTML = ' ';
 
+
+///Loop through each employee and create HTML markup
 employees.forEach((employee, index) => {
     let name = employee.name;
     let email = employee.email;
-    let city = employee.city;
+    let city = employee.location.city;
     let picture = employee.picture;
 
-
+////Template literal to add information to the DOM
 employeeHTML += `
 <div class="card" data-index= "${index}" >
     <img class="avatar" src="${picture.large}" />
         <div class="text-container">
             <h2 class="name">${name.first} ${name.last}</h2>
             <p class="email">${email}</p>
-            <p class="address">${city}</p>
+            <p class="city">${city}</p>
         </div>
 </div>
 `
@@ -44,3 +46,45 @@ employeeHTML += `
 gridContainer.innerHTML = employeeHTML;
 
 }
+
+function displayModal(index) {
+
+////Use Object desructuring make our template literal cleaner
+
+    let { name, dob, phone, email, location: { city, street, state, postcode
+    }, picture } = employees[index];
+
+    let date = new Date(dob.date);
+
+    modalHTML = `
+        <img class="avatar" src="${picture.large}"/>
+        <h2 class="name">${name.first} ${name.last}</h2>
+        <p class="email">${email}</p>
+        <p class="city">${city}</p>
+        <p class="phone">${phone}</p>
+        <p class="address">${street.number} ${street.name} ${state} ${postcode}</p>
+        <p class="bday">Birthday:${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p> 
+        `;
+
+        overlay.classList.remove('hidden');
+        modalContainer.innerHTML = modalHTML;
+}
+
+gridContainer.addEventListener("click", e => {
+
+
+///make sure the clicks is not on the gridContainer itself
+    if(e.target !== gridContainer) {
+
+
+        ///Select the card element based on its proximity to actual elements clicked
+        const card = e.target.closest(".card");
+        const index = card.getAttribute('data-index');
+
+        displayModal(index);
+    }
+});
+
+modalClose.addEventListener('click', () => {
+    overlay.classList.add("hidden");
+})
